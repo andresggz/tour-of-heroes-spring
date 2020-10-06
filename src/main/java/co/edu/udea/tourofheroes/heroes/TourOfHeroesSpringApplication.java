4fa27adc9430ede76e41ef1cdadf4482b1ab84a6
Andres Grisales <andres.grisalesgg@gmail.com>
2020-10-06 03:08:18 -0500

    Initial Spring configuration

---
+++ src/main/java/co/edu/udea/tourofheroes/heroes/TourOfHeroesSpringApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.tourofheroes.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfHeroesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOfHeroesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/tourofheroes/heroes/TourOfHeroesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.tourofheroes.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOfHeroesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
